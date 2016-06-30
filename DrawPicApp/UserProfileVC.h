0432bbcf3619c8e58cf5ad42f275d0cfeb3924eb
Ryuki Uetsugu <u.ryuki1997@gmail.com>
2016-06-30 15:04:00 +0900

    .gitignore追加,ユーザーメニューファイル.h .m追加

---
+++ DrawPicApp/UserProfileVC.h
@@ -0,0 +1,35 @@
+//
+//  UserProfileVC.h
+//  DrawPicApp
+//
+//  Created by  Ryuki Uetsugu on 2016/06/30.
+//  Copyright © 2016年 野村和也. All rights reserved.
+//
+
+//#ifndef UserProfileVC_h
+//#define UserProfileVC_h
+//
+//
+//#endif /* UserProfileVC_h */
+//
+//
+//#import <UIKit/UIKit.h>
+//
+////@interface UserProfileVC : UICollectionViewController
+////
+////@end
+//
+////#import <AWCollec>
+//
+//
+//@interface UserProfileVC : PFQueryCollectionViewController
+//
+//@end
+
+#import <UIKit/UIKit.h>
+
+@interface UserProfileVC : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIView *view;
+
+@end
---
+++ DrawPicApp/UserProfileVC.m
@@ -0,0 +1,29 @@
+//
+//  UserProfileVC.m
+//  DrawPicApp
+//
+//  Created by  Ryuki Uetsugu on 2016/06/30.
+//  Copyright © 2016年 野村和也. All rights reserved.
+//
+
+//@implementation AWCollectionViewDialLayout
+//
+//#import "UserProfile.h"
+//
+//@interface UserProfileVC ()
+//
+//@end
+//
+//@implementation AWCollectionViewDialLayout
+
+#import "UserProfileVC.h"
+#import "AWCollectionViewDialLayout.h"
+
+@interface UserProfileVC ()
+@end
+
+@implementation UserProfileVC
+{
+    UIView *view;
+}
+@end
